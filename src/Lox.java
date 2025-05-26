d9538d1a15dc0fcfdbf319195cd4aabff01dbac3
Renata Costa Rocha <recr17@hotmail.com>
2025-05-26 19:39:36 -0300

    Commit inicial do projeto jlox

---
+++ src/Lox.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes));
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // Por enquanto, só imprima os tokens (depois, passe para o parser)
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // Aqui você pode implementar métodos para reportar erros, se quiser.
+}
