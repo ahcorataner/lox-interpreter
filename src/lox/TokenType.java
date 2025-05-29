f13e98268c5f01cc802f779e950f6d618da4cc17
Renata Costa Rocha <recr17@hotmail.com>
2025-05-28 23:02:08 -0300

    Implementação de tokens (item 4.2). Criados arquivos TokenType.java e Token.java para representar lexemas com tipo, valor literal e linha.

--- src/lox/TokenType.java
+++ src/lox/TokenType.java
@@ -1,5 +1,22 @@
 package lox;
 
-public enum TokenType {
-    // mais pra frente aqui vai os tipos de token
+enum TokenType {
+    // Tokens de um único caractere.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // Um ou dois caracteres.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literais.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Palavras-chave.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
